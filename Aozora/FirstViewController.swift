701963830106cb8eb3259a9c5638f9efd7525b47
Khoren Katklian <casillaskhoren1@yahoo.com>
2018-04-16 10:42:34 +0200

    Initial Commit

---
+++ Aozora/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Aozora
+//
+//  Created by Khoren Katklian on 16/04/2018.
+//  Copyright © 2018 Kusa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
