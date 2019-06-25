a2cec4315db82b10313881f7b88ea14d198accc7
kirito <casillaskhoren1@yahoo.com>
2019-06-25 13:58:08 +0200

    - Initial IAP support

---
+++ Kurozora/Features/In-App Purchases/PurchasesViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PurchasesViewController.swift
+//  Kurozora
+//
+//  Created by Khoren Katklian on 24/06/2019.
+//  Copyright © 2019 Kurozora. All rights reserved.
+//
+
+import UIKit
+
+class PurchaseViewController: UITableViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.theme_backgroundColor = KThemePicker.backgroundColor.rawValue
+	}
+}
+
+// MARK: - UITableViewDataSource
+extension PurchaseViewController {
+
+}
+
+// MARK: -  UITableViewDelegate
+extension PurchaseViewController {
+
+}
