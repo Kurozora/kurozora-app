1b96fea8d7a92dc88f00156f465844b8a03429c4
kirito <casillaskhoren1@yahoo.com>
2019-10-24 15:04:46 +0200

    - Improved "what's new" - Fixed search bar not appearing on explore page

--- Kurozora/Features/WhatsNew/KWhatsNewViewController.swift
+++ Kurozora/Features/WhatsNew/KWhatsNewViewController.swift
@@ -15,4 +15,21 @@ class KWhatsNewViewController: WhatsNewViewController {
 	override var preferredStatusBarStyle: UIStatusBarStyle {
 		return KThemePicker.statusBarStyle.statusBarValue
 	}
+
+	// MARK: - Initializer
+	required init(titleText: String, buttonText: String, items: [WhatsNewItem]) {
+		super.init(items: items)
+		self.titleText = titleText
+		self.buttonText = buttonText
+		self.titleColor = KThemePicker.textColor.colorValue
+		self.view.theme_backgroundColor = KThemePicker.backgroundColor.rawValue
+		self.itemTitleColor = KThemePicker.textColor.colorValue
+		self.itemSubtitleColor = KThemePicker.subTextColor.colorValue
+		self.buttonTextColor = KThemePicker.tintedButtonTextColor.colorValue
+		self.buttonBackgroundColor = KThemePicker.tintColor.colorValue
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+	}
 }
---
+++ Kurozora/Features/WhatsNew/Models/KWhatsNewModel.swift
@@ -0,0 +1,23 @@
+//
+//  KWhatsNewModel.swift
+//  Kurozora
+//
+//  Created by Khoren Katklian on 24/10/2019.
+//  Copyright © 2019 Kurozora. All rights reserved.
+//
+
+import WhatsNew
+
+class KWhatsNewModel {
+	/// Features of the current version of the app. Don't forget to change
+	static var current: [WhatsNewItem] {
+		return v1
+	}
+
+	/// Features of version one of the app.
+	static var v1: [WhatsNewItem] = [
+		.image(title: "Like a Candy", subtitle: "Your app with the colors you like.", image: #imageLiteral(resourceName: "theme_icon")),
+		.image(title: "High Five", subtitle: "Your privacy is our #1 priority!", image: #imageLiteral(resourceName: "privacy_icon")),
+		.image(title: "Attention Grabber", subtitle: "New follower? New message? Look here!", image: #imageLiteral(resourceName: "notifications_icon")),
+	]
+}
