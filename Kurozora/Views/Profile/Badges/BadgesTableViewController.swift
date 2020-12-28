ef70bcf3fa2ae829387da071738e2571d879812c
kirito <casillaskhoren1@yahoo.com>
2020-12-28 22:07:27 +0100

    [Improvements] Badges - Added proper refresh handeling - Added proper empty data view handeling - Added proper data source handeling - Restyled badges table

--- Kurozora/Views/Profile/Badges/BadgesTableViewController.swift
+++ Kurozora/Views/Profile/Badges/BadgesTableViewController.swift
@@ -13,7 +13,10 @@ class BadgesTableViewController: KTableViewController {
 	// MARK: - Properties
 	var badges: [Badge] = [] {
 		didSet {
-			_prefersActivityIndicatorHidden = true
+			self._prefersActivityIndicatorHidden = true
+			self.tableView.reloadData {
+				self.toggleEmptyDataView()
+			}
 		}
 	}
 	var user: User!
@@ -28,40 +31,62 @@ class BadgesTableViewController: KTableViewController {
 		return _prefersActivityIndicatorHidden
 	}
 
+	// Refresh control
+	var _prefersRefreshControlDisabled = false {
+		didSet {
+			self.setNeedsRefreshControlAppearanceUpdate()
+		}
+	}
+	override var prefersRefreshControlDisabled: Bool {
+		return self._prefersRefreshControlDisabled
+	}
+
 	// MARK: - View
     override func viewDidLoad() {
         super.viewDidLoad()
 
+		self._prefersRefreshControlDisabled = true
 		self.badges = self.user.relationships?.badges?.data ?? []
     }
 
 	// MARK: - Functions
 	override func configureEmptyDataView() {
-		tableView.emptyDataSetView { [weak self] (view) in
-			guard let self = self else { return }
+		var detailString: String
 
-			let username = self.user.attributes.username
-			let detailLabelString = self.user?.id != User.current?.id ? "\(username) has no badges to show." : "Badges you earn show up here."
+		if self.user?.id == User.current?.id {
+			detailString = "Badges you earn show up here."
+		} else {
+			detailString = "\(self.user.attributes.username) has not earned any badges yet."
+		}
 
-			view.titleLabelString(NSAttributedString(string: "No Badges", attributes: [.font: UIFont.systemFont(ofSize: 16, weight: .medium), .foregroundColor: KThemePicker.textColor.colorValue]))
-				.detailLabelString(NSAttributedString(string: detailLabelString, attributes: [.font: UIFont.systemFont(ofSize: 16), .foregroundColor: KThemePicker.subTextColor.colorValue]))
-				.image(R.image.empty.badge())
-				.imageTintColor(KThemePicker.textColor.colorValue)
-				.verticalOffset(-50)
-				.verticalSpace(5)
-				.isScrollAllowed(true)
+		emptyBackgroundView.configureImageView(image: R.image.empty.badge()!)
+		emptyBackgroundView.configureLabels(title: "No Badges", detail: detailString)
+
+		tableView.backgroundView?.alpha = 0
+	}
+
+	/// Fades in and out the empty data view according to the number of sections.
+	func toggleEmptyDataView() {
+		if self.tableView.numberOfSections == 0 {
+			self.tableView.backgroundView?.animateFadeIn()
+		} else {
+			self.tableView.backgroundView?.animateFadeOut()
 		}
 	}
 }
 
 // MARK: - UITableViewDataSource
 extension BadgesTableViewController {
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+	override func numberOfSections(in tableView: UITableView) -> Int {
 		return badges.count
-    }
+	}
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 1
+	}
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let identifier = (indexPath.row % 2 == 0) ? R.reuseIdentifier.leftBadgeCell : R.reuseIdentifier.rightBadgeCell
+		let identifier = (indexPath.section % 2 == 0) ? R.reuseIdentifier.leftBadgeCell : R.reuseIdentifier.rightBadgeCell
 		guard let badgeTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath as IndexPath) else {
 			fatalError("Cannot dequeue cell with reuse identifier \(identifier.identifier)")
 		}
@@ -73,6 +98,6 @@ extension BadgesTableViewController {
 extension BadgesTableViewController {
 	override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
 		let badgeTableViewCell = cell as? BadgeTableViewCell
-		badgeTableViewCell?.badge = self.badges[indexPath.row]
+		badgeTableViewCell?.badge = self.badges[indexPath.section]
 	}
 }
