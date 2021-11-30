ba5f609aa5ad8eae694dd7eb9104fb384d980278
kiritokatklian <casillaskhoren1@gmail.com>
2021-11-30 19:23:37 +0100

    [Update] What's New Features - Updated features list on what's new view

--- Kurozora/Features/WhatsNew/Models/KWhatsNewModel.swift
+++ Kurozora/Features/WhatsNew/Models/KWhatsNewModel.swift
@@ -16,8 +16,11 @@ class KWhatsNewModel {
 
 	/// Features of version one of the app.
 	static var v1: [WhatsNewItem] = [
-		.image(title: "Like a Candy", subtitle: "Your app with the colors you like.", image: R.image.icons.theme()!),
-		.image(title: "High Five", subtitle: "Your privacy is our #1 priority!", image: R.image.icons.privacy()!),
-		.image(title: "Attention Grabber", subtitle: "New follower? New message? Look here!", image: R.image.icons.notifications()!)
+		.image(title: "Discovery", subtitle: "Built for discovery. Your anime journey starts with Kurozora!", image: R.image.icons.browser()!),
+		.image(title: "Quick Start", subtitle: "Do you already have a list? Import it in the settings and enjoy!", image: R.image.icons.brands.myAnimeList()!),
+		.image(title: "Like a Candy", subtitle: "Your app in the colors you like.", image: R.image.icons.theme()!),
+		.image(title: "High Five", subtitle: "Your privacy is our #1, #2 and #3 priority!", image: R.image.icons.privacy()!),
+		.image(title: "Attention Grabber", subtitle: "New follower? New message? New show? Look here!", image: R.image.icons.notifications()!),
+		.image(title: "Accounts Switcher", subtitle: "To keep some things separate. We've got you covered 🙈", image: R.image.icons.accountSwitch()!)
 	]
 }
