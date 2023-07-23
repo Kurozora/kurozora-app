cf1b346c79ddd768882b5b2b8307ee6564b7fe3a
kiritokatklian <casillaskhoren1@gmail.com>
2023-07-24 01:32:06 +0200

    [Update] Rating Cell - Removed rating interaction from RatingCollectionViewCell in favor of the new RatingBarCollectionViewCell

--- Kurozora/Resources/Views/Show/Cells/RatingCollectionViewCell.swift
+++ Kurozora/Resources/Views/Show/Cells/RatingCollectionViewCell.swift
@@ -15,10 +15,10 @@ protocol RatingCollectionViewCellDelegate: AnyObject {
 
 class RatingCollectionViewCell: UICollectionViewCell {
 	// MARK: - IBOutlets
-	@IBOutlet weak var cosmosView: KCosmosView!
-	@IBOutlet weak var cosmosDetailLabel: KSecondaryLabel!
-	@IBOutlet weak var ratingLabel: KLabel!
-	@IBOutlet weak var ratingDetailLabel: KSecondaryLabel!
+//	@IBOutlet weak var cosmosView: KCosmosView!
+//	@IBOutlet weak var cosmosDetailLabel: KSecondaryLabel!
+	@IBOutlet weak var primaryLabel: KLabel!
+	@IBOutlet weak var secondaryLabel: KSecondaryLabel!
 
 	// MARK: - Properties
 	weak var delegate: RatingCollectionViewCellDelegate?
@@ -26,101 +26,101 @@ class RatingCollectionViewCell: UICollectionViewCell {
 	// MARK: - Functions
 	/// Configure the cell with the given details.
 	func configure(using show: Show) {
-		// Configure cosmos view
-		let userRating = show.attributes.givenRating
-		self.cosmosView.rating = userRating ?? 0.0
+//		// Configure cosmos view
+//		let userRating = show.attributes.givenRating
+//		self.cosmosView.rating = userRating ?? 0.0
 
-		self.cosmosView.didFinishTouchingCosmos = { rating in
-			WorkflowController.shared.isSignedIn { [weak self] in
-				guard let self = self else { return }
-				self.delegate?.ratingCollectionViewCell(rateWith: rating)
-			}
-
-			if !User.isSignedIn {
-				self.cosmosView.rating = 0.0
-			}
-		}
+//		self.cosmosView.didFinishTouchingCosmos = { rating in
+//			WorkflowController.shared.isSignedIn { [weak self] in
+//				guard let self = self else { return }
+//				self.delegate?.ratingCollectionViewCell(rateWith: rating)
+//			}
+//
+//			if !User.isSignedIn {
+//				self.cosmosView.rating = 0.0
+//			}
+//		}
 
 		// Configure average rating
-		self.ratingLabel.text = "\(show.attributes.stats?.ratingAverage ?? 0.0)"
+		self.primaryLabel.text = "\(show.attributes.stats?.ratingAverage ?? 0.0)"
 
-		// Configure rating count
-		let ratingCount = show.attributes.stats?.ratingCount ?? 0
-		self.cosmosDetailLabel.text = ratingCount != 0 ? "\(ratingCount.kkFormatted) Ratings" : "Not enough ratings"
+//		// Configure rating count
+//		let ratingCount = show.attributes.stats?.ratingCount ?? 0
+//		self.cosmosDetailLabel.text = ratingCount != 0 ? "\(ratingCount.kkFormatted) Ratings" : "Not enough ratings"
 	}
 
 	/// Configure the cell with the given details.
 	func configure(using literature: Literature) {
-		// Configure cosmos view
-		let userRating = literature.attributes.givenRating
-		self.cosmosView.rating = userRating ?? 0.0
-
-		self.cosmosView.didFinishTouchingCosmos = { rating in
-			WorkflowController.shared.isSignedIn { [weak self] in
-				guard let self = self else { return }
-				self.delegate?.ratingCollectionViewCell(rateWith: rating)
-			}
-
-			if !User.isSignedIn {
-				self.cosmosView.rating = 0.0
-			}
-		}
+//		// Configure cosmos view
+//		let userRating = literature.attributes.givenRating
+//		self.cosmosView.rating = userRating ?? 0.0
+//
+//		self.cosmosView.didFinishTouchingCosmos = { rating in
+//			WorkflowController.shared.isSignedIn { [weak self] in
+//				guard let self = self else { return }
+//				self.delegate?.ratingCollectionViewCell(rateWith: rating)
+//			}
+//
+//			if !User.isSignedIn {
+//				self.cosmosView.rating = 0.0
+//			}
+//		}
 
 		// Configure average rating
-		self.ratingLabel.text = "\(literature.attributes.stats?.ratingAverage ?? 0.0)"
+		self.primaryLabel.text = "\(literature.attributes.stats?.ratingAverage ?? 0.0)"
 
-		// Configure rating count
-		let ratingCount = literature.attributes.stats?.ratingCount ?? 0
-		self.cosmosDetailLabel.text = ratingCount != 0 ? "\(ratingCount.kkFormatted) Ratings" : "Not enough ratings"
+//		// Configure rating count
+//		let ratingCount = literature.attributes.stats?.ratingCount ?? 0
+//		self.cosmosDetailLabel.text = ratingCount != 0 ? "\(ratingCount.kkFormatted) Ratings" : "Not enough ratings"
 	}
 
 	/// Configure the cell with the given details.
 	func configure(using game: Game) {
-		// Configure cosmos view
-		let userRating = game.attributes.givenRating
-		self.cosmosView.rating = userRating ?? 0.0
-
-		self.cosmosView.didFinishTouchingCosmos = { rating in
-			WorkflowController.shared.isSignedIn { [weak self] in
-				guard let self = self else { return }
-				self.delegate?.ratingCollectionViewCell(rateWith: rating)
-			}
-
-			if !User.isSignedIn {
-				self.cosmosView.rating = 0.0
-			}
-		}
+//		// Configure cosmos view
+//		let userRating = game.attributes.givenRating
+//		self.cosmosView.rating = userRating ?? 0.0
+//
+//		self.cosmosView.didFinishTouchingCosmos = { rating in
+//			WorkflowController.shared.isSignedIn { [weak self] in
+//				guard let self = self else { return }
+//				self.delegate?.ratingCollectionViewCell(rateWith: rating)
+//			}
+//
+//			if !User.isSignedIn {
+//				self.cosmosView.rating = 0.0
+//			}
+//		}
 
 		// Configure average rating
-		self.ratingLabel.text = "\(game.attributes.stats?.ratingAverage ?? 0.0)"
+		self.primaryLabel.text = "\(game.attributes.stats?.ratingAverage ?? 0.0)"
 
-		// Configure rating count
-		let ratingCount = game.attributes.stats?.ratingCount ?? 0
-		self.cosmosDetailLabel.text = ratingCount != 0 ? "\(ratingCount.kkFormatted) Ratings" : "Not enough ratings"
+//		// Configure rating count
+//		let ratingCount = game.attributes.stats?.ratingCount ?? 0
+//		self.cosmosDetailLabel.text = ratingCount != 0 ? "\(ratingCount.kkFormatted) Ratings" : "Not enough ratings"
 	}
 
 	/// Configure the cell with the episode details.
 	func configure(using episode: Episode) {
-		// Configure cosmos view
-		let userRating = episode.attributes.givenRating
-		self.cosmosView.rating = userRating ?? 0.0
-
-		self.cosmosView.didFinishTouchingCosmos = { rating in
-			WorkflowController.shared.isSignedIn { [weak self] in
-				guard let self = self else { return }
-				self.delegate?.ratingCollectionViewCell(rateWith: rating)
-			}
-
-			if !User.isSignedIn {
-				self.cosmosView.rating = 0.0
-			}
-		}
+//		// Configure cosmos view
+//		let userRating = episode.attributes.givenRating
+//		self.cosmosView.rating = userRating ?? 0.0
+//
+//		self.cosmosView.didFinishTouchingCosmos = { rating in
+//			WorkflowController.shared.isSignedIn { [weak self] in
+//				guard let self = self else { return }
+//				self.delegate?.ratingCollectionViewCell(rateWith: rating)
+//			}
+//
+//			if !User.isSignedIn {
+//				self.cosmosView.rating = 0.0
+//			}
+//		}
 
 		// Configure average rating
-		self.ratingLabel.text = "\(episode.attributes.stats?.ratingAverage ?? 0.0)"
+		self.primaryLabel.text = "\(episode.attributes.stats?.ratingAverage ?? 0.0)"
 
-		// Configure rating count
-		let ratingCount = episode.attributes.stats?.ratingCount ?? 0
-		self.cosmosDetailLabel.text = ratingCount != 0 ? "\(ratingCount.kkFormatted) Ratings" : "Not enough ratings"
+//		// Configure rating count
+//		let ratingCount = episode.attributes.stats?.ratingCount ?? 0
+//		self.cosmosDetailLabel.text = ratingCount != 0 ? "\(ratingCount.kkFormatted) Ratings" : "Not enough ratings"
 	}
 }
