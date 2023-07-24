dc4925e9d4f049e27710a78825be5f06e96414c4
kiritokatklian <casillaskhoren1@gmail.com>
2023-07-24 12:45:38 +0200

    [Update] Rating Cell - Cleaned up comments - Removed RatingCollectionViewCellDelegate in favor of TapToRateCollectionViewCellDelegate

--- Kurozora/Resources/Views/Show/Cells/RatingCollectionViewCell.swift
+++ Kurozora/Resources/Views/Show/Cells/RatingCollectionViewCell.swift
@@ -9,43 +9,15 @@
 import UIKit
 import KurozoraKit
 
-protocol RatingCollectionViewCellDelegate: AnyObject {
-	func ratingCollectionViewCell(rateWith rating: Double)
-}
-
 class RatingCollectionViewCell: UICollectionViewCell {
 	// MARK: - IBOutlets
-//	@IBOutlet weak var cosmosView: KCosmosView!
-//	@IBOutlet weak var cosmosDetailLabel: KSecondaryLabel!
 	@IBOutlet weak var primaryLabel: KLabel!
 	@IBOutlet weak var secondaryLabel: KSecondaryLabel!
-
-	// MARK: - Properties
-	weak var delegate: RatingCollectionViewCellDelegate?
 
 	// MARK: - Functions
 	/// Configure the cell with the given details.
 	func configure(using stats: MediaStat) {
-//		// Configure cosmos view
-//		let userRating = show.attributes.givenRating
-//		self.cosmosView.rating = userRating ?? 0.0
-
-//		self.cosmosView.didFinishTouchingCosmos = { rating in
-//			WorkflowController.shared.isSignedIn { [weak self] in
-//				guard let self = self else { return }
-//				self.delegate?.ratingCollectionViewCell(rateWith: rating)
-//			}
-//
-//			if !User.isSignedIn {
-//				self.cosmosView.rating = 0.0
-//			}
-//		}
-
 		// Configure average rating
 		self.primaryLabel.text = "\(stats.ratingAverage)"
-
-//		// Configure rating count
-//		let ratingCount = show.attributes.stats?.ratingCount ?? 0
-//		self.cosmosDetailLabel.text = ratingCount != 0 ? "\(ratingCount.kkFormatted) Ratings" : "Not enough ratings"
 	}
 }
