ef993df5eff9b3e595b3e762184b9c06476c833b
kiritokatklian <casillaskhoren1@gmail.com>
2023-11-04 02:58:40 +0100

    [Add] Review Lockup - Added ReviewCollectionViewCell - Added Review+UIKit extension

---
+++ Kurozora/App Frameworks/KurozoraKit/Extensions/Models/Review/Review+UIKit.swift
@@ -0,0 +1,112 @@
+//
+//  Review+UIKit.swift
+//  Kurozora
+//
+//  Created by Khoren Katklian on 02/11/2023.
+//  Copyright © 2023 Kurozora. All rights reserved.
+//
+
+import UIKit
+import KurozoraKit
+
+extension Review {
+	func contextMenuConfiguration(in viewController: UIViewController, userInfo: [AnyHashable: Any?])
+	-> UIContextMenuConfiguration? {
+		let identifier = userInfo["identifier"] as? NSCopying
+
+		return UIContextMenuConfiguration(identifier: identifier, previewProvider: nil) { [weak self] _ in
+			guard let self = self else { return nil }
+			return self.makeContextMenu(in: viewController, userInfo: userInfo)
+		}
+	}
+
+	func makeContextMenu(in viewController: UIViewController?, userInfo: [AnyHashable: Any?]) -> UIMenu {
+		var menuElements: [UIMenuElement] = []
+
+		var userMenuElements: [UIMenuElement] = []
+
+		// Username action
+		if let user = self.relationships?.users?.data.first {
+			let username = user.attributes.username
+			let userAction = UIAction(title: "Show " + username + "'s Profile", image: UIImage(systemName: "person.crop.circle.fill")) { [weak self] _ in
+				guard let self = self else { return }
+				self.visitOriginalPosterProfile(from: viewController)
+			}
+			userMenuElements.append(userAction)
+		}
+
+		// Create "share" element
+		let shareAction = UIAction(title: "Share Review", image: UIImage(systemName: "square.and.arrow.up.fill")) { [weak self] _ in
+			guard let self = self else { return }
+			self.openShareSheet(on: viewController)
+		}
+		userMenuElements.append(shareAction)
+
+		// Append user menu
+		menuElements.append(UIMenu(title: "", options: .displayInline, children: userMenuElements))
+
+		if User.isSignedIn {
+			// Report message action
+			var reportMenuElements: [UIMenuElement] = []
+			let reportAction = UIAction(title: "Report Message", image: UIImage(systemName: "exclamationmark.circle.fill"), attributes: .destructive) { [weak self] _ in
+				guard let self = self else { return }
+				self.reportMessage()
+			}
+			reportMenuElements.append(reportAction)
+
+			// Append report menu
+			menuElements.append(UIMenu(title: "", options: .displayInline, children: reportMenuElements))
+		}
+
+		// Create and return a UIMenu
+		return UIMenu(title: "", children: menuElements)
+	}
+
+	/// Presents the profile view for the message poster.
+	///
+	/// - Parameter viewController: The view controller initiaing the segue.
+	func visitOriginalPosterProfile(from viewController: UIViewController? = UIApplication.topViewController) {
+		guard let user = self.relationships?.users?.data.first else { return }
+
+		let profileTableViewController = ProfileTableViewController.`init`(with: user)
+		profileTableViewController.dismissButtonIsEnabled = true
+
+		let kurozoraNavigationController = KNavigationController(rootViewController: profileTableViewController)
+		viewController?.present(kurozoraNavigationController, animated: true)
+	}
+
+	/// Present share sheet for the review.
+	///
+	/// Make sure to send either the view or the bar button item that's sending the request.
+	///
+	/// - Parameters:
+	///    - viewController: The view controller presenting the share sheet.
+	///    - view: The `UIView` sending the request.
+	///    - barButtonItem: The `UIBarButtonItem` sending the request.
+	func openShareSheet(on viewController: UIViewController? = UIApplication.topViewController, _ view: UIView? = nil, barButtonItem: UIBarButtonItem? = nil) {
+		var shareText = "\"\(self.attributes.description ?? "")\""
+		if let user = self.relationships?.users?.data.first {
+			shareText += "-\(user.attributes.username)"
+		}
+
+		let activityItems: [Any] = [shareText]
+		let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: [])
+
+		if let popoverController = activityViewController.popoverPresentationController {
+			if let view = view {
+				popoverController.sourceView = view
+				popoverController.sourceRect = view.frame
+			} else {
+				popoverController.barButtonItem = barButtonItem
+			}
+		}
+		viewController?.present(activityViewController, animated: true, completion: nil)
+	}
+
+	/// Sends a report of the selected message to the mods.
+	func reportMessage() {
+		WorkflowController.shared.isSignedIn {
+			UIApplication.topViewController?.presentAlertController(title: "Review Reported", message: "Thank you for helping keep the community safe.")
+		}
+	}
+}
--- Kurozora/Resources/Views/Show/Cells/ReviewCollectionViewCell.swift
+++ Kurozora/Resources/Views/Show/Cells/ReviewCollectionViewCell.swift
@@ -7,10 +7,130 @@
 //
 
 import UIKit
+import KurozoraKit
 
-class ReviewCollectionViewCell: UICollectionViewCell {
-	@IBOutlet weak var reviewerButton: KButton!
-	@IBOutlet weak var scoreLabel: KSecondaryLabel!
+protocol ReviewCollectionViewCellDelegate: AnyObject {
+	func reviewCollectionViewCell(_ cell: ReviewCollectionViewCell, didPressUserName sender: AnyObject)
+	func reviewCollectionViewCell(_ cell: ReviewCollectionViewCell, didPressProfileBadge button: UIButton, for profileBadge: ProfileBadge)
+}
+
+class ReviewCollectionViewCell: KCollectionViewCell {
+	// MARK: - IBOutlets
+	@IBOutlet weak var profileImageView: ProfileImageView!
+	@IBOutlet weak var usernameLabel: KLabel!
+	@IBOutlet weak var profileBadgeStackView: ProfileBadgeStackView!
+	@IBOutlet weak var dateTimeLabel: KSecondaryLabel!
 	@IBOutlet weak var cosmosView: KCosmosView!
-	@IBOutlet weak var reviewLabel: KLabel!
+	@IBOutlet weak var contentTextView: KTextView!
+
+	// MARK: - Properties
+	weak var delegate: ReviewCollectionViewCellDelegate?
+
+	// MARK: - Initializers
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		self.sharedInit()
+	}
+
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+		self.sharedInit()
+	}
+
+	// MARK: - Functions
+	/// The shared settings used to initialize the cell.
+	fileprivate func sharedInit() {
+		self.theme_backgroundColor = KThemePicker.tableViewCellBackgroundColor.rawValue
+		self.layerCornerRadius = 8
+	}
+
+	/// Configure the cell with the given person details.
+	func configureCell(using review: Review?) {
+		guard let review = review else {
+			self.showSkeleton()
+			return
+		}
+		self.hideSkeleton()
+
+		if let user = review.relationships?.users?.data.first {
+			self.usernameLabel.text = user.attributes.username
+			user.attributes.profileImage(imageView: self.profileImageView)
+
+			// Attach gestures
+			self.configureProfilePageGesture(for: self.usernameLabel)
+			self.configureProfilePageGesture(for: self.profileImageView)
+
+			// Badges
+			self.profileBadgeStackView.delegate = self
+			self.profileBadgeStackView.configure(for: user)
+		}
+
+		// Configure rating
+		self.cosmosView.rating = review.attributes.score
+
+		// Configure body
+		self.contentTextView.setAttributedText(review.attributes.description?.markdownAttributedString())
+		self.contentTextView.delegate = self
+
+		// Configure date time
+		self.dateTimeLabel.text = review.attributes.createdAt.formatted(date: .abbreviated, time: .omitted)
+	}
+
+	/// Adds a `UITapGestureRecognizer` which opens the profile image onto the given view.
+	///
+	/// - Parameter view: The view to which the tap gesture should be attached.
+	fileprivate func configureProfilePageGesture(for view: UIView) {
+		if view.gestureRecognizers.isNilOrEmpty {
+			let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(usernameLabelPressed(_:)))
+			gestureRecognizer.numberOfTouchesRequired = 1
+			gestureRecognizer.numberOfTapsRequired = 1
+			view.addGestureRecognizer(gestureRecognizer)
+			view.isUserInteractionEnabled = true
+		}
+	}
+
+	fileprivate func getUserIdentity(username: String) async -> UserIdentity? {
+		do {
+			let userIdentityResponse = try await KService.searchUsers(for: username).value
+			return userIdentityResponse.data.first
+		} catch {
+			print("-----", error.localizedDescription)
+			return nil
+		}
+	}
+
+	// MARK: - IBActions
+	@objc func usernameLabelPressed(_ sender: AnyObject) {
+		self.delegate?.reviewCollectionViewCell(self, didPressUserName: sender)
+	}
+}
+
+// MARK: - UITextViewDelegate
+extension ReviewCollectionViewCell: UITextViewDelegate {
+	func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+		if URL.absoluteString.starts(with: "https://kurozora.app/profile") {
+			Task { [weak self] in
+				guard let self = self else { return }
+				let username = URL.lastPathComponent
+				guard let userIdentity = await self.getUserIdentity(username: username) else { return }
+				let deeplink = URL.absoluteString
+					.replacingOccurrences(of: "https://kurozora.app/", with: "kurozora://")
+					.replacingOccurrences(of: username, with: "\(userIdentity.id)")
+					.url
+
+				UIApplication.shared.kOpen(nil, deepLink: deeplink)
+			}
+
+			return false
+		}
+
+		return true
+	}
+}
+
+// MARK: - ProfileBadgeStackViewDelegate
+extension ReviewCollectionViewCell: ProfileBadgeStackViewDelegate {
+	func profileBadgeStackView(_ view: ProfileBadgeStackView, didPress button: UIButton, for profileBadge: ProfileBadge) {
+		self.delegate?.reviewCollectionViewCell(self, didPressProfileBadge: button, for: profileBadge)
+	}
 }
