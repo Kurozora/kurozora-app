0e8429f7973b83bf832b1146cd0fd9feef81aa09
kiritokatklian <casillaskhoren1@gmail.com>
2024-12-13 23:51:49 +0100

    [Update] Broadcast Label - Added docs

--- Kurozora/App Frameworks/UIKit/Views and Controls/Text Views/BroadcastLabel.swift
+++ Kurozora/App Frameworks/UIKit/Views and Controls/Text Views/BroadcastLabel.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
+/// A themed view that displays one or more lines of read-only text, often used in conjunction with controls to describe their intended purpose.
+///
+/// The color of the labels is pre-configured with the currently selected theme's secondary text color.
+/// You can add labels to your interface programmatically or by using Interface Builder.
+/// The view also contains a countdown timer that updates every second.
+///
+/// - Tag: BroadcastLabel
 class BroadcastLabel: KTintedLabel {
+	// MARK: - Properties
+	/// The timer that updates the countdown label.
 	private var timer: Timer?
+
+	/// The target date of the countdown.
 	private var targetDate: Date?
+
+	/// The target duration of the countdown.
 	private var targetDuration: TimeInterval?
 
+	// MARK: - Initializers
+	deinit {
+		self.stopCountdown()
+	}
+
+	// MARK: - Functions
+	/// Starts a countdown to a given date and duration.
+	///
+	/// - Parameters:
+	///  - date: The date to countdown to.
+	///  - duration: The duration of the countdown.
 	func startCountdown(to date: Date, duration: TimeInterval) {
 		self.targetDate = date
 		self.targetDuration = duration
 		self.updateCountdown()
 		self.timer?.invalidate()
 
-		// Schedule a new timer
 		self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
 			guard let self = self else { return }
 			self.updateCountdown()
@@ -31,6 +54,7 @@ class BroadcastLabel: KTintedLabel {
 		}
 	}
 
+	/// Updates the countdown label.
 	private func updateCountdown() {
 		guard let targetDate = self.targetDate else { return }
 		guard let targetDuration = self.targetDuration else { return }
@@ -44,13 +68,9 @@ class BroadcastLabel: KTintedLabel {
 		}
 	}
 
+	/// Stops the countdown timer.
 	func stopCountdown() {
 		self.timer?.invalidate()
 		self.timer = nil
-	}
-
-	deinit {
-		// Ensure timer is invalidated when label is deallocated
-		self.stopCountdown()
 	}
 }
