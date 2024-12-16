e4d4e296e057d96a8c0ec8e34ccdb952626f8117
kiritokatklian <casillaskhoren1@gmail.com>
2024-12-16 01:18:57 +0100

    [Update] Browse Category - Added support for specifying `segueIdentifier`

--- Kurozora/App/Models/BrowseCategory.swift
+++ Kurozora/App/Models/BrowseCategory.swift
@@ -13,15 +13,30 @@ struct BrowseCategory: Hashable {
 	// MARK: - Properties
 	let title: String
 	let image: UIImage?
-	let searchType: KKSearchType
+	let searchType: KKSearchType?
+	let segueIdentifier: String?
+
+	// MARK: - Initializers
+	init(
+		title: String,
+		image: UIImage?,
+		searchType: KKSearchType? = nil,
+		segueIdentifier: String? = nil
+	) {
+		self.title = title
+		self.image = image
+		self.searchType = searchType
+		self.segueIdentifier = segueIdentifier
+	}
 
 	// MARK: - Functions
 	static func == (lhs: BrowseCategory, rhs: BrowseCategory) -> Bool {
-		lhs.title == rhs.title && lhs.searchType == rhs.searchType
+		lhs.title == rhs.title && lhs.searchType == rhs.searchType && lhs.segueIdentifier == rhs.segueIdentifier
 	}
 
 	func hash(into hasher: inout Hasher) {
 		hasher.combine(self.title)
 		hasher.combine(self.searchType)
+		hasher.combine(self.segueIdentifier)
 	}
 }
