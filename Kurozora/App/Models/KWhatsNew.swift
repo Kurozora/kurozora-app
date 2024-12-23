556cae1086bf27412c8472c38af16fa2111ae826
kiritokatklian <casillaskhoren1@gmail.com>
2024-12-23 01:12:00 +0100

    [Update] What's New - Added new `What's New` tekst

--- Kurozora/App/Models/KWhatsNew.swift
+++ Kurozora/App/Models/KWhatsNew.swift
@@ -11,8 +11,21 @@ import WhatsNew
 class KWhatsNew {
 	/// Features of the current version of the app. Don't forget to change
 	static var current: [WhatsNewItem] {
-		return v110
+		return v111
 	}
+
+	/// Features of version 1.11.0 of the app.
+	static var v111: [WhatsNewItem] = [
+		.image(title: "Cozy Cocoa", subtitle: "Bring festive cheer to your home screen with the Christmas Kuro-chan app icon, and Can D. Cane premium theme.", image: R.image.icons.gift()!),
+		.image(title: "Sticker Wonderland", subtitle: "Added 6 new Kuro-chan stickers for a total of 24. Spread joy while messaging!", image: R.image.icons.kuroChanStickerSignal()!),
+		.image(title: "New Re:CAP Design", subtitle: "A refreshed look to better reflect your year-end memories.", image: R.image.icons.session()!),
+		.image(title: "Your Schedule Awaits", subtitle: "Introducing the new Schedule view for quick access to daily episodes and releases.", image: R.image.icons.calendar()!),
+		.image(title: "Fresh Feed", subtitle: "A revamped feed design to keep your updates looking polished and sleek.", image: R.image.icons.personCropCircle()!),
+		.image(title: "Profile Makeover", subtitle: "Edit your username, bio, banner, and more with the redesigned Profile Edit page.", image: R.image.icons.personCropCircleGear()!),
+		.image(title: "Quick Access", subtitle: "Tapping the Date widget now takes you straight to the title’s details page.", image: R.image.icons.widget()!),
+		.image(title: "Seamless Sign-Ins", subtitle: "Actions requiring login are now performed automatically after you sign in.", image: R.image.icons.faceID()!),
+		.image(title: "Bug Fixes", subtitle: "Poured a cup of hot cocoa on those pesky bugs, resolving issues like deep links opening without a close button, the 'Go to Last Watched' episode feature misbehaving, separator theming in badge sections, and more. Your experience should now be smoother than ever!", image: R.image.icons.bug()!)
+	]
 
 	/// Features of version 1.10.0 of the app.
 	static var v110: [WhatsNewItem] = [
@@ -119,51 +132,5 @@ class KWhatsNew {
 		.image(title: "Game On!", subtitle: "Stay Ahead of the Game! Game tracking on Kurozora is now a breeze. Explore a wide variety of anime-related games, keep track of your favorites, and never miss a beat on the latest anime-inspired titles. Expand your universe beyond TV and books!", image: R.image.icons.game()!),
 		.image(title: "UAL", subtitle: "With PRO and Kurozora+ you can now use the new Unified Anime Linking feature. It supports up to 12 services!", image: R.image.icons.mention()!),
 		.image(title: "Jump-start", subtitle: "Now you can easily import your anime and manga list from other services directly into Kurozora, streamlining your tracking experience with just a few *taps*!", image: R.image.icons.brands.myAnimeList()!)
-	]
-
-	/// Features of version 1.3.3 of the app.
-	static var v133: [WhatsNewItem] = [
-		.image(title: "Hippity Hop", subtitle: "Get ready to hop into spring with the Eggatha app icon! It’s back and ready to spread Easter cheer together with our new Pastel Paradise theme. And don’t miss out on the Easter special anime and manga recommendations!", image: R.image.icons.gift()!),
-		.image(title: "Track your manga journey", subtitle: "Explore a vast universe of manga, light novels and other literature titles! Keep track of your reading progress and history while never missing a single chapter!", image: R.image.icons.manga()!),
-		.image(title: "Game On!", subtitle: "Stay Ahead of the Game! Game tracking on Kurozora is now a breeze. Explore a wide variety of anime-related games, keep track of your favorites, and never miss a beat on the latest anime-inspired titles. Expand your universe beyond TV and books!", image: R.image.icons.game()!),
-		.image(title: "UAL", subtitle: "With PRO and Kurozora+ you can now use the new Unified Anime Linking feature. It supports up to 12 services!", image: R.image.icons.mention()!),
-		.image(title: "Jump-start", subtitle: "Now you can easily import your anime and manga list from other services directly into Kurozora, streamlining your tracking experience with just a few *taps*!", image: R.image.icons.brands.myAnimeList()!)
-	]
-
-	/// Features of version 1.3 of the app.
-	static var v13: [WhatsNewItem] = [
-		.image(title: "White Day Delight", subtitle: "Celebrate White Day with our heartfelt app icon, delectable White Chocolate theme, and our new Romantic Reminders anime and manga recommendations!", image: R.image.icons.gift()!),
-		.image(title: "Track your manga journey", subtitle: "Explore a vast universe of manga, light novels and other literature titles! Keep track of your reading progress and history while never missing a single chapter!", image: R.image.icons.manga()!),
-		.image(title: "Game On!", subtitle: "Stay Ahead of the Game! Game tracking on Kurozora is now a breeze. Explore a wide variety of anime-related games, keep track of your favorites, and never miss a beat on the latest anime-inspired titles. Expand your universe beyond TV and books!", image: R.image.icons.game()!),
-		.image(title: "UAL", subtitle: "With PRO and Kurozora+ you can now use the new Unified Anime Linking feature. It supports up to 12 services!", image: R.image.icons.mention()!),
-		.image(title: "Jump-start", subtitle: "Now you can easily import your anime and manga list from other services directly into Kurozora, streamlining your tracking experience with just a few *taps*!", image: R.image.icons.brands.myAnimeList()!)
-	]
-
-	/// Features of version 1.2 of the app.
-	static var v12: [WhatsNewItem] = [
-		.image(title: "Happy Valentine’s", subtitle: "Fall in love with our Valentine’s Day update: Love Bug app icon and theme. Don’t forget to check out the Love to Bits anime recommendation.", image: R.image.icons.gift()!),
-		.image(title: "UAL", subtitle: "With PRO and Kurozora+ you can now use the new Unified Anime Linking feature. It supports up to 12 services!", image: R.image.icons.mention()!),
-		.image(title: "Track your manga journey", subtitle: "Anime isn’t enough for you? Or are you more of a reader? No matter what your reason is, Kurozora has you covered. You can now keep track of your manga reading progress and history, rate and never forget where you left off. Try it now!", image: R.image.icons.manga()!),
-		.image(title: "Jump-start", subtitle: "Now you can easily import your anime and manga list from other services directly into Kurozora, streamlining your tracking experience with just a few *taps*!", image: R.image.icons.brands.myAnimeList()!)
-	]
-
-	/// Features of version 1.1 of the app.
-	static var v11: [WhatsNewItem] = [
-		.image(title: "Merry Christmas", subtitle: "Are you in the spirit for Christmas? Check the setting for the new Meri-Chri app icon and Cozy Cabin theme.", image: R.image.icons.gift()!),
-		.image(title: "Mentions", subtitle: "You can now mention users in the feed, so they never miss your messages.", image: R.image.icons.mention()!),
-		.image(title: "Reminders", subtitle: "Kurozora+ users can now opt-in to personalized iCal subscriptions to be notified the next time their favorite anime is about to air! This can be done in the in-app setting.", image: R.image.icons.reminder()!),
-		.image(title: "Placeholders", subtitle: "Out with the old and in with the new. The new placeholders are a treat to look at!", image: R.image.icons.placeholder()!),
-		.image(title: "Account Badges", subtitle: "Are you a Kurozora+ member? You can wear your PRO+ badge with pride. But, if that's not enough for you, you can also get a verification badge! No monthly subscriptions required 😉", image: R.image.icons.badge()!),
-		.image(title: "Song Details", subtitle: "You can now tap on a song to view its details, such as what shows it played on. You can also open songs directly in Apple Music through the share menu at the top.", image: R.image.icons.music()!)
-	]
-
-	/// Features of version 1.0 of the app.
-	static var v10: [WhatsNewItem] = [
-		.image(title: "Discovery", subtitle: "Built for discovery. Your anime journey starts with Kurozora!", image: R.image.icons.browser()!),
-		.image(title: "Quick Start", subtitle: "Do you already have a list? Import it in the settings and enjoy!", image: R.image.icons.brands.myAnimeList()!),
-		.image(title: "Like a Candy", subtitle: "Your app in the colors you like.", image: R.image.icons.theme()!),
-		.image(title: "High Five", subtitle: "Your privacy is our #1, #2 and #3 priority!", image: R.image.icons.privacy()!),
-		.image(title: "Attention Grabber", subtitle: "New follower? New message? New show? Look here!", image: R.image.icons.notifications()!),
-		.image(title: "Accounts Switcher", subtitle: "To keep some things separate. We've got you covered 🙈", image: R.image.icons.accountSwitch()!)
 	]
 }
