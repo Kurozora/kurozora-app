c06f8bb7c7c1a9305d2e8a01066bef1e4e6f4266
kiritokatklian <casillaskhoren1@gmail.com>
2025-01-26 23:57:17 +0100

    chore: color extension concurrency - Changed `var` to `let`

--- Kurozora/App Frameworks/UIKit/Extensions/UIColor+Kurozora.swift
+++ Kurozora/App Frameworks/UIKit/Extensions/UIColor+Kurozora.swift
@@ -11,70 +11,70 @@ import UIKit
 // MARK: - General
 extension UIColor {
 	/// Aozora purple color of value `#8C50A3`.
-	static var aozora: UIColor = #colorLiteral(red: 0.5490196078, green: 0.3137254902, blue: 0.6392156863, alpha: 1)
+	static let aozora: UIColor = #colorLiteral(red: 0.5490196078, green: 0.3137254902, blue: 0.6392156863, alpha: 1)
 
 	/// Kurozora orange color of value `#FF9300`.
-	static var kurozora: UIColor = #colorLiteral(red: 1, green: 0.5764705882, blue: 0, alpha: 1)
+	static let kurozora: UIColor = #colorLiteral(red: 1, green: 0.5764705882, blue: 0, alpha: 1)
 
 	/// Kurozora green color of value `#37D321`.
-	static var kGreen: UIColor = #colorLiteral(red: 0.2156862745, green: 0.8274509804, blue: 0.1294117647, alpha: 1)
+	static let kGreen: UIColor = #colorLiteral(red: 0.2156862745, green: 0.8274509804, blue: 0.1294117647, alpha: 1)
 
 	/// Kurozora light red color of value `#FF4158`.
-	static var kLightRed: UIColor = #colorLiteral(red: 1, green: 0.2549019608, blue: 0.3450980392, alpha: 1)
+	static let kLightRed: UIColor = #colorLiteral(red: 1, green: 0.2549019608, blue: 0.3450980392, alpha: 1)
 
 	/// Kurozora dark red color of value `#D0021B`.
-	static var kDarkRed: UIColor = #colorLiteral(red: 0.8156862745, green: 0.007843137255, blue: 0.1058823529, alpha: 1)
+	static let kDarkRed: UIColor = #colorLiteral(red: 0.8156862745, green: 0.007843137255, blue: 0.1058823529, alpha: 1)
 
 	/// Kurozora yellow color value `#FDD35E`.
-	static var kYellow: UIColor = #colorLiteral(red: 0.9921568627, green: 0.8274509804, blue: 0.368627451, alpha: 1)
+	static let kYellow: UIColor = #colorLiteral(red: 0.9921568627, green: 0.8274509804, blue: 0.368627451, alpha: 1)
 
 	/// Kurozora charcoal color value `#353A50`.
-	static var kGrayishNavy: UIColor = #colorLiteral(red: 0.2078431373, green: 0.2274509804, blue: 0.3137254902, alpha: 1)
+	static let kGrayishNavy: UIColor = #colorLiteral(red: 0.2078431373, green: 0.2274509804, blue: 0.3137254902, alpha: 1)
 }
 
 // MARK: - External Sites
 extension UIColor {
 	/// Anilist color value `#262F3E`.
-	static var anilist: UIColor = #colorLiteral(red: 0.1490196078, green: 0.1843137255, blue: 0.2431372549, alpha: 1)
+	static let anilist: UIColor = #colorLiteral(red: 0.1490196078, green: 0.1843137255, blue: 0.2431372549, alpha: 1)
 
 	/// Crunchyroll color value `#F78B2E`.
-	static var crunchyroll: UIColor = #colorLiteral(red: 0.968627451, green: 0.5450980392, blue: 0.1803921569, alpha: 1)
+	static let crunchyroll: UIColor = #colorLiteral(red: 0.968627451, green: 0.5450980392, blue: 0.1803921569, alpha: 1)
 
 	/// Daisuki color value `#111111`.
-	static var daisuki: UIColor = #colorLiteral(red: 0.06666666667, green: 0.06666666667, blue: 0.06666666667, alpha: 1)
+	static let daisuki: UIColor = #colorLiteral(red: 0.06666666667, green: 0.06666666667, blue: 0.06666666667, alpha: 1)
 
 	/// Funimation color value `#1C1D1D`.
-	static var funimation: UIColor = #colorLiteral(red: 0.1098039216, green: 0.1137254902, blue: 0.1137254902, alpha: 1)
+	static let funimation: UIColor = #colorLiteral(red: 0.1098039216, green: 0.1137254902, blue: 0.1137254902, alpha: 1)
 
 	/// Hummingbird color value `#EC8661`.
-	static var hummingbird: UIColor = #colorLiteral(red: 0.9254901961, green: 0.5254901961, blue: 0.3803921569, alpha: 1)
+	static let hummingbird: UIColor = #colorLiteral(red: 0.9254901961, green: 0.5254901961, blue: 0.3803921569, alpha: 1)
 
 	/// MyAnimeList color value `#2E51A2`.
-	static var myAnimeList: UIColor = #colorLiteral(red: 0.1803921569, green: 0.3176470588, blue: 0.6352941176, alpha: 1)
+	static let myAnimeList: UIColor = #colorLiteral(red: 0.1803921569, green: 0.3176470588, blue: 0.6352941176, alpha: 1)
 
 	/// Official site color value `#FDD35E`.
-	static var officialSite: UIColor = #colorLiteral(red: 0.9907178283, green: 0.8274499178, blue: 0.3669273257, alpha: 1)
+	static let officialSite: UIColor = #colorLiteral(red: 0.9907178283, green: 0.8274499178, blue: 0.3669273257, alpha: 1)
 
 	/// Other color value `#77C344`.
-	static var other: UIColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+	static let other: UIColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
 }
 
 // MARK: - Library status
 extension UIColor {
 	/// Green color of value `#56FF43`.
-	static var watching: UIColor = #colorLiteral(red: 0.337254902, green: 1, blue: 0.262745098, alpha: 1)
+	static let watching: UIColor = #colorLiteral(red: 0.337254902, green: 1, blue: 0.262745098, alpha: 1)
 
 	/// Blue color of value `#03A9F4`.
-	static var completed: UIColor = #colorLiteral(red: 0.01176470588, green: 0.662745098, blue: 0.9568627451, alpha: 1)
+	static let completed: UIColor = #colorLiteral(red: 0.01176470588, green: 0.662745098, blue: 0.9568627451, alpha: 1)
 
 	/// Yellow color of value `#F5B433`.
-	static var planning: UIColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+	static let planning: UIColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
 
 	/// Red color of value `#FF4158`.
-	static var onHold: UIColor = #colorLiteral(red: 1, green: 0.2549019608, blue: 0.3450980392, alpha: 1)
+	static let onHold: UIColor = #colorLiteral(red: 1, green: 0.2549019608, blue: 0.3450980392, alpha: 1)
 
 	/// Gray color of value `#646464`.
-	static var dropped: UIColor = #colorLiteral(red: 0.3921568627, green: 0.3921568627, blue: 0.3921568627, alpha: 1)
+	static let dropped: UIColor = #colorLiteral(red: 0.3921568627, green: 0.3921568627, blue: 0.3921568627, alpha: 1)
 }
 
 // MARK: - Helpers
