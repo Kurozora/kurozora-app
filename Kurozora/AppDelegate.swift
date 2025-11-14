f993d6922a6c1d1882e7f327c6f697f46f71336b
kiritokatklian <casillaskhoren1@gmail.com>
2025-11-14 17:14:45 +0100

    chore: cleanup `AppDelegate` - Removed deprecated and unused methods

--- Kurozora/AppDelegate.swift
+++ Kurozora/AppDelegate.swift
@@ -39,28 +39,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		return true
 	}
 
-	func applicationWillResignActive(_ application: UIApplication) {
-		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-		// Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-		print("----- UIApplication will resign active.")
-	}
-
-	func applicationDidEnterBackground(_ application: UIApplication) {
-		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-		print("----- UIApplication entered background.")
-	}
-
-	func applicationWillEnterForeground(_ application: UIApplication) {
-		// Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-		print("----- UIApplication will enter foreground.")
-	}
-
-	func applicationDidBecomeActive(_ application: UIApplication) {
-		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-		print("----- UIApplication became active.")
-	}
-
 	func applicationWillTerminate(_ application: UIApplication) {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 		print("----- UIApplication will terminate.")
@@ -70,7 +48,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 // MARK: - UIScene
 extension AppDelegate {
-	// Here we tell iOS what scene configuration to use
 	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
 		print("----- UIApplication connecting to scene session.")
 		connectingSceneSession.userInfo?["activity"] = options.userActivities.first?.activityType
