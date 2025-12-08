a009125a6acb76a7de63ab427affa5fcddaa0188
kiritokatklian <casillaskhoren1@gmail.com>
2025-12-08 22:52:11 +0100

    refactor: separate `AccountCell` from `SettingsCell`

---
+++ Kurozora/Resources/Views/Settings/Cells/AccountCell.swift
@@ -0,0 +1,11 @@
+//
+//  AccountCell.swift
+//  Kurozora
+//
+//  Created by Khoren Katklian on 08/12/2025.
+//  Copyright © 2025 Kurozora. All rights reserved.
+//
+
+import UIKit
+
+class AccountCell: SettingsCell {}
