5ac6d5cad557f1f8a22b1b91fd2717fade47b922
kiritokatklian <casillaskhoren1@gmail.com>
2025-12-25 00:18:41 +0100

    refactor: consolidate delegate and data source extensions - Moved `UICollectionViewController` and `UITableViewController` delegate and data source methods into `KCollectionViewController` and `KTableViewController` respectively

--- Kurozora/App Frameworks/UIKit/View Controllers/Content View Controllers/Collection Views/KCollectionViewController/Data/KCollectionViewDataSource.swift
+++ Kurozora/App Frameworks/UIKit/View Controllers/Content View Controllers/Collection Views/KCollectionViewController/Data/KCollectionViewDataSource.swift
@@ -17,3 +17,38 @@ import UIKit
 	@objc optional func updateDataSource()
 	@MainActor @objc optional func configureDataSource()
 }
+
+// MARK: - KCollectionViewController
+extension KCollectionViewController: KCollectionViewDataSource {
+	/// Asks your data source object for the cells registered with the `collectionView`.
+	///
+	/// If you do not implement this method, the collection view uses a default value of an empty array.
+	///
+	/// - Parameter collectionView: The collection view requesting this information.
+	///
+	/// - Returns: The collection view cells registered with `collectionView`.
+	///
+	/// - Tag: KCollectionViewDataSource-registerCellsForCollectionView
+	func registerCells(for collectionView: UICollectionView) -> [UICollectionViewCell.Type] {
+		return []
+	}
+
+	/// Asks your data source object for the reusable views registered with the `collectionView`.
+	///
+	/// If you do not implement this method, the collection view uses a default value of an empty array.
+	///
+	/// - Parameter collectionView: The collection view requesting this information.
+	///
+	/// - Returns: The collection view reusable views registered with `collectionView`.
+	///
+	/// - Tag: KCollectionViewDataSource-registerNibsForCollectionView
+	func registerNibs(for collectionView: UICollectionView) -> [UICollectionReusableView.Type] {
+		return []
+	}
+
+	/// Asks your data source object to configure the data source of the collection view.
+	func configureDataSource() {}
+
+	/// Asks your data source object to update the data source of the collection view.
+	func updateDataSource() {}
+}
