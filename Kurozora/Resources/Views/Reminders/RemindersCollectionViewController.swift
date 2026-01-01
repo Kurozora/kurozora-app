f714dfc5810c9d6e5ac9e708335d036252b92d24
kiritokatklian <casillaskhoren1@gmail.com>
2026-01-01 03:30:17 +0100

    refactor: migrate Reminders storyboard setup to code - Deleted `Reminders.storyboard` - Refactored `RemindersCollectionViewController` for programmatic setup

--- Kurozora/Resources/Views/Reminders/RemindersCollectionViewController.swift
+++ Kurozora/Resources/Views/Reminders/RemindersCollectionViewController.swift
@@ -9,9 +9,7 @@
 import UIKit
 import KurozoraKit
 
-class RemindersCollectionViewController: KCollectionViewController, StoryboardInstantiable {
-	static var storyboardName: String = "Reminders"
-
+class RemindersCollectionViewController: KCollectionViewController {
 	// MARK: - Enums
 	enum SegueIdentifiers: String, SegueIdentifier {
 		case showDetailsSegue
@@ -48,6 +46,9 @@ class RemindersCollectionViewController: KCollectionViewController, StoryboardIn
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+
+		self.title = Trans.myReminders
+
 		// Observe NotificationCenter for an update.
 		NotificationCenter.default.addObserver(self, selector: #selector(self.fetchRemindersList), name: .KReminderModelsListDidChange, object: nil)
 
@@ -122,7 +123,7 @@ class RemindersCollectionViewController: KCollectionViewController, StoryboardIn
 		}
 
 		do {
-			let reminderLibraryResponse = try await KService.getReminders(for: self.libraryKind, next: self.nextPageURL).value
+			let reminderLibraryResponse = try await KService.getReminders(for: self.libraryKind, next: self.nextPageURL, limit: self.nextPageURL != nil ? 100 : 25).value
 
 			// Reset data if necessary
 			if self.nextPageURL == nil {
@@ -166,23 +167,30 @@ class RemindersCollectionViewController: KCollectionViewController, StoryboardIn
 	}
 
 	// MARK: - Segue
-	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-		guard
-			let segueIdentifier = segue.identifier,
-			let segueID = SegueIdentifiers(rawValue: segueIdentifier)
-		else { return }
+	override func makeDestination(for identifier: SegueIdentifier) -> UIViewController? {
+		guard let segue = identifier as? SegueIdentifiers else { return nil }
 
-		switch segueID {
+		switch segue {
+		case .showDetailsSegue: return ShowDetailsCollectionViewController()
+		case .literatureDetailsSegue: return LiteratureDetailsCollectionViewController()
+		case .gameDetailsSegue: return GameDetailsCollectionViewController()
+		}
+	}
+
+	override func prepare(for identifier: any SegueIdentifier, destination: UIViewController, sender: Any?) {
+		guard let identifier = identifier as? SegueIdentifiers else { return }
+
+		switch identifier {
 		case .showDetailsSegue:
-			guard let showDetailsCollectionViewController = segue.destination as? ShowDetailsCollectionViewController else { return }
+			guard let showDetailsCollectionViewController = destination as? ShowDetailsCollectionViewController else { return }
 			guard let show = sender as? Show else { return }
 			showDetailsCollectionViewController.show = show
 		case .literatureDetailsSegue:
-			guard let literatureDetailCollectionViewController = segue.destination as? LiteratureDetailsCollectionViewController else { return }
+			guard let literatureDetailCollectionViewController = destination as? LiteratureDetailsCollectionViewController else { return }
 			guard let literature = sender as? Literature else { return }
 			literatureDetailCollectionViewController.literature = literature
 		case .gameDetailsSegue:
-			guard let gameDetailCollectionViewController = segue.destination as? GameDetailsCollectionViewController else { return }
+			guard let gameDetailCollectionViewController = destination as? GameDetailsCollectionViewController else { return }
 			guard let game = sender as? Game else { return }
 			gameDetailCollectionViewController.game = game
 		}
