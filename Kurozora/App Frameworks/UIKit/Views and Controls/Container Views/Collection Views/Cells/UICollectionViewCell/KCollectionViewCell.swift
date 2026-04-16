94f65e69bb3927abdfe7d51df727ebbd11b7ea6c
kiritokatklian <casillaskhoren1@gmail.com>
2026-04-16 22:20:16 +0200

    chore: cleanup KCollectionViewCell - Removed unused `Alamofire` import - Cleaned up redundant `dataRequest` property and its cancellation calls

--- Kurozora/App Frameworks/UIKit/Views and Controls/Container Views/Collection Views/Cells/UICollectionViewCell/KCollectionViewCell.swift
+++ Kurozora/App Frameworks/UIKit/Views and Controls/Container Views/Collection Views/Cells/UICollectionViewCell/KCollectionViewCell.swift
@@ -8,25 +8,12 @@
 
 import UIKit
 import KurozoraKit
-import Alamofire
 
 class KCollectionViewCell: UICollectionViewCell, SkeletonDisplayable {
-	// MARK: - Properties
-	/// The request which handles in-memory Data download using URLSessionDataTask.
-	var dataRequest: DataRequest?
-
-	// MARK: - Initializers
-	deinit {
-		self.dataRequest?.cancel()
-		self.dataRequest = nil
-	}
-
 	// MARK: - View
 	override func prepareForReuse() {
 		super.prepareForReuse()
 
-		self.dataRequest?.cancel()
-		self.dataRequest = nil
 		self.showSkeleton()
 	}
 }
